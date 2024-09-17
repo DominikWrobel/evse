ff7dd6fbec0a8da90277eb1743712e1974bb92a9
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-09-17 17:23:42 +0200

    Create const.py
    v0.1

---
+++ custom_components/evse/const.py
@@ -0,0 +1,10 @@
+"""Constants for the EVSE integration."""
+DOMAIN = "evse"
+PLATFORMS = ["sensor", "switch"]
+
+DEFAULT_NAME = "EVSE"
+DEFAULT_SCAN_INTERVAL = 10
+DEFAULT_PORT = 80
+
+CONF_HOST = "host"
+CONF_PORT = "port"
