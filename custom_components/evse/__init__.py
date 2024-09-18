65e58c3e19d4c7c800b4172edd64adc1ff65ea47
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-09-18 12:59:37 +0200

    Update __init__.py
    v0.6

--- custom_components/evse/__init__.py
+++ custom_components/evse/__init__.py
@@ -1,11 +1,24 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceEntryType
+from homeassistant.helpers import device_registry as dr
 from .const import DOMAIN, PLATFORMS
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     """Set up EVSE integration from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
+
+    # Create a device entry
+    device_registry = dr.async_get(hass)
+    device_registry.async_get_or_create(
+        config_entry_id=config_entry.entry_id,
+        identifiers={(DOMAIN, config_entry.unique_id)},
+        name=config_entry.data['name'],
+        manufacturer="EVSE",
+        model="EVSE Controller",
+        entry_type=DeviceEntryType.SERVICE
+    )
 
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
